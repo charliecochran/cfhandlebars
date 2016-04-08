25b2b653c88e0a2a1eea48020f0cba8728c38042
Charlie Cochran <charlie.cochran@gmail.com>
2016-04-07 19:15:58 -0500

    Add roadmap for parser build process

---
+++ build.js
@@ -0,0 +1,21 @@
+console.log('Building...');
+
+var Parser = require('./handlebars.js');
+
+// PARSER STUFF
+
+// INSERT variables (except for o function)
+// INSERT this.symbols_
+// INSERT this.terminals_
+// INSERT this.productions_
+// INSERT this.table
+// INSERT this.defaultActions
+// performAction function body replace this. with yyval.
+
+
+// LEXER STUFF
+
+// INSERT rules / replace regexp objects with regexp strings
+// INSERT conditions
+// replace _BACKSLASH_ with \
+// performAction function body
